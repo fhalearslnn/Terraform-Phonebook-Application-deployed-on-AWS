aa19f887ab419f8ce293b44f6dd603553b292018
Hale Arslan <75578501+fhalearslnn@users.noreply.github.com>
2024-01-19 12:12:24 +0300

    Updated

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -6,7 +6,7 @@ dnf install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 dnf install git -y
-TOKEN="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"
+TOKEN="xxxxxxxxxxxxxxxxxxxxxxxx" #write here your token
 cd /Terraform-Phonebook-Application-deployed-on-AWS/ && git clone  https://$TOKEN@github.com/fhalearslnn/Terraform-Phonebook-Application-deployed-on-AWS.git
 # python3 /home/ec2-user/phonebook/phonebook-app.py
 python3 /devops-projects/Terraform-Phonebook-Application-deployed-on-AWS/phonebook-app.py
